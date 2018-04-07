2de79adbe556711aa3736e64b9920a24fe99b5cf
Phrida Norrhall <phridanorrhall@gmail.com>
2018-04-07 18:05:41 +0200

    Almost able to get Twitter status from Kafka

--- src/main/scala/TweetGenerator.scala
+++ src/main/scala/TweetGenerator.scala
@@ -1,34 +1,23 @@
-import java.lang.StringBuffer
+import java.io._
 import java.text.SimpleDateFormat
-import java.util
-import java.util.Properties
-import java.util.stream.Collectors
 
+import twitter4j._
 import net.liftweb.json._
-import org.apache.commons.configuration.ConfigurationFactory.ConfigurationBuilder
-
-import scala.collection.JavaConverters
-import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
-import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.mllib.classification.NaiveBayesModel
-import org.apache.spark.rdd.RDD
-import org.apache.spark.serializer.KryoSerializer
-import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaUtils}
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
-import twitter4j.{FilterQuery, Status, TwitterStreamFactory}
-import utils.PropertiesLoader
 
-import scala.collection.immutable.HashMap
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scalaj.http.{Http, HttpResponse}
+
+import scala.collection.immutable.HashMap
+import scala.io.Source
+
 
 
 object TweetGenerator extends Serializable {
 
-  var hasRelatedKeywords = false
   def main(args: Array[String]): Unit = {
 
     //Inställningar för twitter
@@ -38,123 +27,110 @@ object TweetGenerator extends Serializable {
     System.setProperty("twitter4j.oauth.accessTokenSecret", "W3uxu0BdpqIhuByjaa2xWXm2Ae6yFuIofI4dTyKzz87wa")
 
 
-    val sparkContext = new SparkContext()
-    val stopEvent = false
-    var streamingContext = Option.empty[StreamingContext]
-    var shouldReload = false
-
     val zkQuorum = "localhost:2181"
     val kafkaParams = Map("metadata.broker.list" -> "localhost:9092")
     val group = "my-group"
     val twitterTopic = Set("twitterdata")
     val numThreads = 1
     //val Array(zkQuorum, group, topics, numThreads) = args;
-    val sparkConf = new SparkConf().setAppName("TweetGenerator")
-    //val ssc = new StreamingContext(sparkConf, Seconds(2))
-    //ssc.sparkContext.setLogLevel("OFF")
-
-
-
-    val processThread = new Thread() {
-      override def run(): Unit = {
-        while (!stopEvent) {
-          if (streamingContext.isEmpty && !shouldReload) {
-            streamingContext = Option(new StreamingContext(sparkContext, Seconds(1)))
-            streamingContext.get.sparkContext.setLogLevel("OFF")
-
-            val kafkaTweets = KafkaUtils.createStream(streamingContext.get, zkQuorum, group, Map("twitterdata" -> 1)).map(_._2)
-            //val kafkaTweets = TwitterUtils.createStream(streamingContext.get, None)
-            val kafkaKeywords = KafkaUtils.createStream(streamingContext.get, zkQuorum, group, Map("keyworddata" -> 1)).map(_._2)
-            //val kafkaKeywords = streamingContext.get.socketTextStream("localhost", 8888)
-            val words = kafkaKeywords.flatMap(_.split(" ")).toString
-            kafkaKeywords.print()
-            val filter = "Trump"
-
-            //val relatedKeywords = getRelatedKeywords(words)
-
-            val filterTweets = kafkaTweets.filter(_.contains(filter))
-            filterTweets.print()
-            streamingContext.get.start()
-          } else {
-            if (shouldReload) {
-              streamingContext.get.stop(false, true)
-              streamingContext.get.awaitTermination()
-              streamingContext = Option.empty[StreamingContext]
-              shouldReload = false
-            } else {
-              Thread.sleep(1000)
-            }
-          }
-        }
-
-        streamingContext.get.stop(true, true)
-        streamingContext.get.awaitTermination()
-      }
-    }
-
-    processThread.start()
-    processThread.join()
-
-
-    //Inställningar för Kafka
-
-/*
-    //Inställningar för elasticsearch
+    val sparkConf = new SparkConf().setAppName("TweetGenerator").setMaster("local[*]")
     sparkConf.set("es.index.auto.create", "true")
     sparkConf.set("es.resource", "visualization/tweets")
     sparkConf.set("es.nodes", "localhost")
     sparkConf.set("es.port", "9200")
     sparkConf.set("es.nodes.discovery", "true")
-    sparkConf.set("spark.serializer", classOf[KryoSerializer].getName)
-    val index_name = "twitter"*/
+    val index_name = "twitter"
+    var ssc = Option.empty[StreamingContext]
+    ssc = Option(new StreamingContext(sparkConf, Seconds(2)))
+    ssc.get.sparkContext.setLogLevel("OFF")
 
-    /*
-    //Kafka topic för att ta emot Tweets
-    val kafkaTweets = KafkaUtils.createStream(ssc, zkQuorum, group, Map("twitterdata" -> 1)).map(_._2)
+    val initialFile = new ListBuffer[String]
+    initialFile.append("Initial")
+    writeToFile(initialFile)
+    println("Initial file: " + readFromFile())
 
-    //Kafka topic för att ta emot keywords
-    val kafkaKeywords = KafkaUtils.createStream(ssc, zkQuorum, group, Map("keyworddata" -> 1)).map(_._2)
+    val filterLines = KafkaUtils.createStream(ssc.get, zkQuorum, group, Map("keyworddata" -> 1)).map(_._2)
+    val filterWords = filterLines.flatMap(_.split(" "))
+    filterWords.print()
 
-    //kafkaKeywords.print()
+    filterWords.foreachRDD(rdd => {
+      rdd.foreach(word => {
+        val words = new ListBuffer[String]
 
-    val words = kafkaKeywords.flatMap(_.split(" "))
-
-
-
-
-    //Hämta ut relaterade ord för varje keyword från Google
-    val relatedKeywords = getRelatedKeywords(words)
-
-    kafkaKeywords.foreachRDD(rdd => {
-      println("Here now")
-      //ssc.stop(false, true)
-      val kafkaTweetsNew = KafkaUtils.createStream(ssc, zkQuorum, group, Map("twitterdata" -> 1)).map(_._2)
-      kafkaTweetsNew.filter(_.contains(relatedKeywords))
-      kafkaTweetsNew.print()
-      hasRelatedKeywords = false
-    })
-*/
-
-
-
-
-
-
-    //val testArray = new ArrayBuffer[String]()
-    //testArray.append("Obama")
-
-    //Hämtar ut Tweets direkt från Twitter. Fördelen är att man kan sätta upp en HashMap som kan användas i Elasticsearch.
-    /*
-    val query = new FilterQuery()
-    val filter = finalArray.mkString(",")
-    query.track(filter)
-
-    val tweets = TwitterUtils.createFilteredStream(ssc, None, Some(query))
-
-    val tweetMap = tweets.map(status => {
-      finalArray.foreach(word => {
-        println("Contains keyword: " + (status.getText.contains(word) || status.getHashtagEntities().map(_.getText).contains(word)))
+        println("Word to append: " + word)
+        words.append(word)
+        println("Words in ListBuffer: " + words)
+        writeToFile(words)
+        println("Words in file: " + readFromFile().toList)
       })
+    })
+
+    val tweets = KafkaUtils.createStream(ssc.get, zkQuorum, group, Map("twitterdata" -> 1)).map(_._2)
+
+    //tweets.print()
+    //val tweets = TwitterUtils.createStream(ssc.get, None)
+
+    val filteredTweets = tweets.filter(_.contains(readFromFile().toList.head))
+
+    //filteredTweets.print()
+
+
+
+    filteredTweets.foreachRDD(rdd => {
+      rdd.foreachPartition(partitionOfRecords => {
+        //println("Status: " + partitionOfRecords.getClass)
+        while (partitionOfRecords.hasNext) {
+          val tweet = partitionOfRecords.next()
+          println("TweetOriginal: " + tweet.getClass)
+          val changedTweet = tweet.replace("retweetedStatus", "retweeted_status")
+          println("Changed Tweet" + changedTweet)
+
+          val status = TwitterObjectFactory.createStatus(changedTweet)
+          println("TweetStatus: " + status)
+
+
+          /*
+
+          val hashtags = status.getHashtagEntities().map(_.getText)
+          val formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+
+          val tweetMap = HashMap(
+            "userID" -> status.getUser.getId(),
+            "userScreenName" -> status.getUser.getScreenName(),
+            "userName" -> status.getUser.getName(),
+            "userDescription" -> status.getUser.getDescription(),
+            "message" -> status.getText(),
+            "messageLength" -> status.getText.length(),
+            "hashtags" -> hashtags.mkString(" "),
+            //"createdAt" -> formatter.format(status.getCreatedAt.getTime()),
+            "friendsCount" -> status.getUser.getFriendsCount(),
+            "followersCount" -> status.getUser.getFollowersCount(),
+            "coordinates" -> Option(status.getGeoLocation).map(geo => {s"${geo.getLatitude},${geo.getLongitude}"}),
+            "placeCountry" -> Option(status.getPlace).map(place => {s"${place.getCountry}"}),
+            "userLanguage" -> status.getUser.getLang,
+            "statusLanguage" -> status.getLang
+          )
+          println("TweetMap: " + tweetMap)*/
+        }
+
+      })
+    })
+
+
+
+
+
+    //val filteredTweets = tweets.filter(_.contains(readFromFile().toList.head))
+
+    //filteredTweets.print()
+
+    //val tweetStatus: Status = TwitterObjectFactory.createStatus(filteredTweets.toString)
+
+    //println("User: "+ tweetStatus.getUser)
+
+    /*
+    val tweetMap = filteredTweets.map(status => {
+
       val hashtags = status.getHashtagEntities().map(_.getText)
       val formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
 
@@ -174,11 +150,34 @@ object TweetGenerator extends Serializable {
         //"userLanguage" -> status.getUser.getLang,
         //"statusLanguage" -> status.getLang
       )
-    })*/
+    })
+
+    tweetMap.print()*/
 
 
-    //ssc.start()
-    //ssc.awaitTermination()
+
+
+
+    //filteredTweets.foreachRDD { tweet => EsSpark.saveToEs(tweet, "visualization/tweets")}
+    //tweetMap.foreachRDD{ tweet => EsSpark.saveToEs(tweet, "visualization/tweets")
+    //filteredTweets.print()
+
+
+
+
+
+    /*
+        if (readFromFile().toList.nonEmpty) {
+          val filter = readFromFile().toList.head
+          println("Filter: " + filter)
+          val filterTweets = tweets.filter(_.contains(filter))
+          filterTweets.print()
+        }*/
+
+
+    ssc.get.start()
+    ssc.get.awaitTermination()
+
   }
 
   def getGoogleList(keyword: String): List[String] = {
@@ -186,13 +185,12 @@ object TweetGenerator extends Serializable {
     val response: HttpResponse[String] = Http("https://kgsearch.googleapis.com/v1/entities:search")
       .params(Seq("query" -> keyword.toString, "limit" -> 10.toString, "key" -> "AIzaSyAR0679Of_1TcUWQhgQS-_7hYSSv3SnE8s"))
       .asString
-
     val json = parse(response.body)
     val listObject = json \\ "name"
     val list: List[String] = listObject \\ classOf[JString]
     list
   }
-
+/*
   def getRelatedKeywords(words: DStream[String]): List[String] = {
     val wordArray = new ArrayBuffer[String]()
     val finalArray = new ArrayBuffer[String]()
@@ -208,12 +206,35 @@ object TweetGenerator extends Serializable {
           }
         })
         println("ArrayFinal: " + finalArray)
-        hasRelatedKeywords = true
 
       })
     })
     finalArray.toList
+  }*/
+
+  def writeToFile(list: ListBuffer[String]): Any = {
+
+    val file = new File("/Users/Phrida/IdeaProjects/Masterprosjekt/src/main/scala/resources/output.txt")
+    println(file.exists())
+    val writer = new PrintWriter(file)
+    //new FileOutputStream(file,true)
+    //val finalList = getGoogleList(list.toList.head)
+    //println("Finallist: " + finalList)
+    list.foreach(word => {
+      writer.append(word)
+    })
+
+    writer.close()
+    //println("Formatted: " + readFromFile().flatMap(_.split(",")).toList)
   }
+
+  def readFromFile(): ListBuffer[String] = {
+    /*val resource = getClass.getResourceAsStream("/resources/output.txt")
+    Source.fromInputStream(resource).getLines().to[ListBuffer]*/
+    Source.fromFile("/Users/Phrida/IdeaProjects/Masterprosjekt/src/main/scala/resources/output.txt").getLines.to[ListBuffer]
+  }
+
+
 
 
 
