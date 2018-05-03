614c38dd2058d5c6f084e3fc1b3d223fdb5eed45
Phrida Norrhall <phridanorrhall@gmail.com>
2018-05-03 16:36:27 +0200

    updates

--- src/main/scala/Statistics.scala
+++ src/main/scala/Statistics.scala
@@ -5,15 +5,17 @@ import java.util.TimerTask
 class Statistics extends TimerTask {
 
   var count: Long = 0L
+  var total: Long = _
   var startTime: Long = System.currentTimeMillis()
-  var file: Path = Paths.get("/Users/Phrida/PlotTest/tps.csv")
-  Files.write(file, "TIME,\tTPS".getBytes(), StandardOpenOption.APPEND)
+  var file: Path = Paths.get("/home/spnorrha/PlotResults/tps.csv")
+  Files.write(file, "TIME,\tTPS\n".getBytes(), StandardOpenOption.APPEND)
 
   def log(count: Long): Unit = {
-    println("Start time: " + startTime)
+    //println("Start time: " + startTime)
     var timeDelta = (System.currentTimeMillis() - startTime) / 1000
-    println("Time delta: " + timeDelta)
+    //println("Time delta: " + timeDelta)
     val logEntry = timeDelta.toString + ",\t" + count.toString + "\n"
+    //println("LogEntry: " + logEntry)
 
     try {
       Files.write(file, logEntry.getBytes(), StandardOpenOption.APPEND)
@@ -30,7 +32,19 @@ class Statistics extends TimerTask {
 
 
   override def run(): Unit = {
-    log(count);
-    count = 0L;
+
+    //For batchsize = 3
+    /*if (count > 0) {
+      total = count/3
+      log(total)
+      count = 0L
+    } else if (count == 0)  {
+      log(total)
+    }*/
+
+    //For batchsize = 1
+    log(count)
+    count=0L
+
   }
 }
--- src/main/scala/utils/PropertiesLoader.scala
+++ src/main/scala/utils/PropertiesLoader.scala
@@ -2,11 +2,11 @@ package utils
 
 object PropertiesLoader {
 
-  val sentiment140TrainingSetFilePath = "/Users/phrida/TweetsSentiment/trainingandtestdata/training.1600000.processed.noemoticon.csv"
-  val sentiment140TestingFilePath = "/Users/phrida/TweetsSentiment/trainingandtestdata/testdata.manual.2009.06.14.csv"
+  val sentiment140TrainingSetFilePath = "/home/spnorrha/IdeaProjects/TweetsSentiment/trainingandtestdata/training.1600000.processed.noemoticon.csv"
+  val sentiment140TestingFilePath = "/home/spnorrha/IdeaProjects/TweetsSentiment/trainingandtestdata/testdata.manual.2009.06.14.csv"
   //val stopWords = "/Users/phrida/IdeaProjects/TwitterVisualizationProject/src/main/scala/StopWord_Corpus.txt"
-  val naiveBayesModelPath = "/Users/phrida/TweetsSentiment/NBModel"
-  val modelAccuracyPath = "/Users/phrida/TweetsSentiment/Accuracy"
-  val keywordsFilePath = "/Users/phrida/Downloads/keywords.csv"
-  val stopWordsFilePath = "/Users/phrida/TweetsSentiment/trainingandtestdata/StopWord_Corpus.txt"
+  val naiveBayesModelPath = "/home/spnorrha/IdeaProjects/TweetsSentiment/NBModel"
+  val modelAccuracyPath = "/home/spnorrha/IdeaProjects/TweetsSentiment/Accuracy"
+  val keywordsFilePath = "/home/spnorrha/IdeaProjects/Downloads/keywords.csv"
+  val stopWordsFilePath = "/home/spnorrha/IdeaProjects/TweetsSentiment/trainingandtestdata/StopWord_Corpus.txt"
 }
